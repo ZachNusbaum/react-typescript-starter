40f921a49d4d620cb1c1a1fad59303f460cd843f
Zach Nusbaum <zach@zdnmail.com>
2019-04-05 21:54:55 -0700

    Add FontAwesome

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 const HelloWorld = (props: any) => {
   let { name } = props;
-  return <h1>Hello, {name}!</h1>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faCoffee} />
+      <h1>Hello, {name}!</h1>
+    </div>
+  );
 };
 
 export default HelloWorld;
